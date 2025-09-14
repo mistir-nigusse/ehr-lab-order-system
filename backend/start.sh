6ceb89483bc40909d2b6affd08e77630bd7974e2
mistir-nigusse <mistirnigusse0@gmail.com>
2025-09-14 18:41:37 +0200

    Deployment: added render.yaml and gunicorn

--- backend/start.sh
+++ backend/start.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 set -e
 
+PORT_TO_USE=${PORT:-5000}
+
 echo "[backend] running migrations"
 alembic upgrade head
 
-echo "[backend] starting flask"
-exec flask --app app:create_app run --host=0.0.0.0 --port=5000
+echo "[backend] starting gunicorn on port ${PORT_TO_USE}"
+exec gunicorn -b 0.0.0.0:${PORT_TO_USE} -w ${WEB_CONCURRENCY:-2} 'app:create_app()'
